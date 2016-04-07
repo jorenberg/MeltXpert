784288694376f070b4178f38e15bdc559a8aaf68
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-08 05:21:02 +0530

    Initial commit
    MeltXpert®, [v0.0.1] - C Programming Language Code.
    Standard: ANSI C.

---
+++ src/meltxpert.h
@@ -0,0 +1,8 @@
+/*
+ * MeltXpert®
+ * ----------
+ * Author     : Prabhat Kumar, http://prabhatkumar.org/.
+ * Copyright  : 2006 - 2016, Prabhat Kumar, All rights reserved.
+ * Date       : 04/09/2006
+ * -------------------------------------------------------------
+ */
