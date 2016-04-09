7b418276c7dd93afbfe606f4f4ebbfea2c23b2ff
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-09 21:19:21 +0530

    Initial commit
    ultraviolet.js, The AMD JavaScript - for defining "MeltXpert Ultraviolet Engine".

---
+++ source/core/script/module/ultraviolet.js
@@ -0,0 +1,20 @@
+/*!
+ * MeltXpert®
+ * ____________________________________________________________________
+ * AXIX®, https://github.com/AXIX
+ * The AMD JavaScript - for defining "MeltXpert Ultraviolet Engine".
+ * ____________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ____________________________________________________________________
+ * @date      : 09-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * ____________________________________________________________________
+ *
+ * --/The Heart of AMD System/-- of "MeltXpert®".
+ * ____________________________________________________________________
+ */
