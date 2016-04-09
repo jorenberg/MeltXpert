2dbf96505fec682b631e381467c7885dd35c5c07
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-09 17:31:12 +0530

    Initial commit
    Contributors of MeltXpert®.

--- source/core/fonts/compare.py
+++ source/core/fonts/compare.py
@@ -5,3 +5,4 @@ __authors__ 	= [
     '"Prabhat Kumar" <prabhat.genome@gmail.com>',
     '"Sequømics Corporation" <admin@sequomics.com>'
     ]
+__contributors__ = '"Emma Jörenberg" <https://github.com/jorenberg>'
