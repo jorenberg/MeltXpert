edc45128380f72cbf59c1de872b8f0f10a8eafa3
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-11 00:42:19 +0530

    Initial commit
    d3-hierarchy: Layout algorithms for visualizing hierarchical data.
    Help: https://github.com/d3/d3-hierarchy

---
+++ source/core/script/plugin/d3/d3.hierarchy.js
@@ -0,0 +1,18 @@
+/*!
+ * ~~~~~~~~~~~~~
+ * d3-hierarchy® : Layout algorithms for visualizing hierarchical data.
+ * ___________________________________________________________________________
+ * @copyright : 2010 - 2016, Mike Bostock, All Rights Reserved.
+ * @help      : https://github.com/d3/d3-hierarchy
+ * @license   : MIT
+ * @require   : requirejs
+ * ___________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ___________________________________________________________________________
+ *
+ * --/The Heart of AMD System/-- of "MeltXpert®".
+ * ___________________________________________________________________________
+ */
