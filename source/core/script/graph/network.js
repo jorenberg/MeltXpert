2624367e06c1776609f46bab6677aa2884330436
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-10 08:01:39 +0530

    Initial commit
    network.js, The AMD JavaScript - for defining "network" configurations.

---
+++ source/core/script/graph/network.js
@@ -0,0 +1,20 @@
+/*!
+ * MeltXpert®
+ * _______________________________________________________________
+ * AXIX®, https://github.com/AXIX
+ * The AMD JavaScript - for defining "network" configurations.
+ * _______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * _______________________________________________________________
+ * @date      : 10-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * _______________________________________________________________
+ *
+ * --/The Heart of AMD System/-- of "MeltXpert®".
+ * _______________________________________________________________
+ */
