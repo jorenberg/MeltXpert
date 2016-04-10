0f46ece1325d64d5245e664e075b347d7b1fd729
Sequømics <sequomics@users.noreply.github.com>
2016-04-10 11:42:46 +0530

    Initial commit
    google.js, The Core JavaScript - for Non-AMD "Google Analytics".

---
+++ source/core/script/helpers/google.js
@@ -0,0 +1,19 @@
+/*!
+ * MeltXpert®
+ * _______________________________________________________________
+ * The Core JavaScript - for Non-AMD "Google Analytics".
+ * _______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * _______________________________________________________________
+ * @date      : 10-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * _______________________________________________________________
+ *
+ * --/The Heart of Non-AMD System/-- of "MeltXpert®".
+ * _______________________________________________________________
+ */
