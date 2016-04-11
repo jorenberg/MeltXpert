1c79c395569e7f0be56189b64d41bc7840ce53a7
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-11 14:06:39 +0530

    Initial commit
    d3-color: Color spaces! RGB, HSL, Cubehelix, Lab (CIELAB) and HCL (CIELCH).
    Help: https://github.com/d3/d3-color

---
+++ source/core/script/plugin/d3/d3.color.js
@@ -0,0 +1,18 @@
+/*!
+ * ~~~~~~~~~
+ * d3-color®  : Color spaces! RGB, HSL, Cubehelix, Lab (CIELAB) and HCL (CIELCH).
+ * __________________________________________________________________________________
+ * @copyright : 2010 - 2015, Mike Bostock, All Rights Reserved.
+ * @help      : https://github.com/d3/d3-color
+ * @license   : MIT
+ * @require   : requirejs
+ * __________________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * __________________________________________________________________________________
+ *
+ * --/The Heart of AMD System/-- of "MeltXpert®".
+ * __________________________________________________________________________________
+ */
