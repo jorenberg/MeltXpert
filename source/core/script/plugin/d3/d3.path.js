95a02fcca0d34b94d0d3b0d6cff16dd9a7b721d5
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-11 14:18:05 +0530

    Initial commit
    d3-path: Serialize Canvas path commands to SVG.
    Help: https://github.com/d3/d3-path

---
+++ source/core/script/plugin/d3/d3.path.js
@@ -0,0 +1,18 @@
+/*!
+ * ~~~~~~~~
+ * d3-path®   : Serialize Canvas path commands to SVG.
+ * __________________________________________________________________________________
+ * @copyright : 2010 - 2015, Mike Bostock, All Rights Reserved.
+ * @help      : https://github.com/d3/d3-path
+ * @license   : MIT
+ * @require   : requirejs
+ * __________________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * __________________________________________________________________________________
+ *
+ * --/The Heart of AMD System/-- of "MeltXpert®".
+ * __________________________________________________________________________________
+ */
