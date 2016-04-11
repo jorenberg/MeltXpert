15b2daf553aa0d37e9ec1c8b89bdab43c9ed6b6d
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-11 16:04:00 +0530

    Initial commit
    d3-stack: A presentation library with intuitive, scroll-based navigation.
    Help: https://github.com/mbostock/stack

---
+++ source/core/script/plugin/d3/d3.stack.js
@@ -0,0 +1,18 @@
+/*!
+ * ~~~~~~~~~
+ * d3-stack®  : A presentation library with intuitive, scroll-based navigation.
+ * __________________________________________________________________________________
+ * @copyright : 2012, Mike Bostock, All Rights Reserved.
+ * @help      : https://github.com/mbostock/stack
+ * @license   : MIT
+ * @require   : requirejs
+ * __________________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * __________________________________________________________________________________
+ *
+ * --/The Heart of AMD System/-- of "MeltXpert®".
+ * __________________________________________________________________________________
+ */
