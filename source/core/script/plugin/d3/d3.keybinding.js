f4319a12d7a5081f192b1eed60f1483dd500800c
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-11 15:24:02 +0530

    Initial commit
    d3-keybinding: Keybindings for d3.
    Help: https://github.com/d3/d3-plugins

---
+++ source/core/script/plugin/d3/d3.keybinding.js
@@ -0,0 +1,21 @@
+/*!
+ * ~~~~~~~~~~~~~~
+ * d3-keybinding® : Keybindings for d3.
+ * __________________________________________________________________________________
+ * @copyright : 2011 - 2013, Mike Bostock, All Rights Reserved.
+ * @help      : https://github.com/d3/d3-plugins
+ * @license   : MIT
+ * @require   : requirejs
+ * __________________________________________________________________________________
+ * @Reference : https://github.com/keithamus/jwerty
+ * @Reference : https://github.com/madrobby/keymaster
+ * __________________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * __________________________________________________________________________________
+ *
+ * --/The Heart of AMD System/-- of "MeltXpert®".
+ * __________________________________________________________________________________
+ */
