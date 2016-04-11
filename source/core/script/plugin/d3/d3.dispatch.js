f978e3b5eb4d84fd6225b9c06b344ac894a77ddf
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-11 14:10:42 +0530

    Initial commit
    d3-dispatch: Register named callbacks and call them with arguments.
    Help: https://github.com/d3/d3-dispatch

---
+++ source/core/script/plugin/d3/d3.dispatch.js
@@ -0,0 +1,18 @@
+/*!
+ * ~~~~~~~~~~~~
+ * d3-dispatch® : Register named callbacks and call them with arguments.
+ * __________________________________________________________________________________
+ * @copyright : 2010 - 2015, Mike Bostock, All Rights Reserved.
+ * @help      : https://github.com/d3/d3-dispatch
+ * @license   : MIT
+ * @require   : requirejs
+ * __________________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * __________________________________________________________________________________
+ *
+ * --/The Heart of AMD System/-- of "MeltXpert®".
+ * __________________________________________________________________________________
+ */
