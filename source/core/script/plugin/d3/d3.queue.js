8f8db84765b4258d73d7672c230f1dec7aaa48f0
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-11 14:36:09 +0530

    Initial commit
    d3-queue: Evaluate asynchronous tasks with configurable concurrency.
    help: https://github.com/d3/d3-queue

---
+++ source/core/script/plugin/d3/d3.queue.js
@@ -0,0 +1,18 @@
+/*!
+ * ~~~~~~~~~
+ * d3-queue®  : Evaluate asynchronous tasks with configurable concurrency.
+ * __________________________________________________________________________________
+ * @copyright : 2012 - 2016, Mike Bostock, All Rights Reserved.
+ * @help      : https://github.com/d3/d3-queue
+ * @license   : MIT
+ * @require   : requirejs
+ * __________________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * __________________________________________________________________________________
+ *
+ * --/The Heart of AMD System/-- of "MeltXpert®".
+ * __________________________________________________________________________________
+ */
