c56c05f95a15b309cf0b0b9b6535abc16b01ba9a
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-11 14:31:39 +0530

    Initial commit
    d3-selection: Transform the DOM by selecting elements and joining to data.
    Help: https://github.com/d3/d3-selection

---
+++ source/core/script/plugin/d3/d3.selection.js
@@ -0,0 +1,18 @@
+/*!
+ * ~~~~~~~~~~~~~
+ * d3-selection® : Transform the DOM by selecting elements and joining to data.
+ * __________________________________________________________________________________
+ * @copyright : 2010 - 2015, Mike Bostock, All Rights Reserved.
+ * @help      : https://github.com/d3/d3-selection
+ * @license   : MIT
+ * @require   : requirejs
+ * __________________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * __________________________________________________________________________________
+ *
+ * --/The Heart of AMD System/-- of "MeltXpert®".
+ * __________________________________________________________________________________
+ */
