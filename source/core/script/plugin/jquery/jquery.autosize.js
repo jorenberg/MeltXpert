0cd45c846a648410c012d784b351cfc1dbad857e
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-12 22:32:59 +0530

    Initial commit
    jQuery-Autosize: Autosize, to automatically adjust textarea height to fit text.
    Help: https://github.com/jackmoore/autosize

---
+++ source/core/script/plugin/jquery/jquery.autosize.js
@@ -0,0 +1,19 @@
+/*!
+ * ~~~~~~~~~~~~~~~~
+ * jQuery-Autosize® : Autosize, to automatically adjust textarea height to fit text.
+ * __________________________________________________________________________________
+ * @copyright : 2015, Jack Moore, All Rights Reserved.
+ * @help      : https://github.com/jackmoore/autosize
+ * @license   : MIT
+ * @require   : jQuery
+ * @require   : requirejs
+ * __________________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * __________________________________________________________________________________
+ *
+ * --/The Heart of AMD System/-- of "MeltXpert®".
+ * __________________________________________________________________________________
+ */
