0cc6b1e28a26d7e6da2bb55171fbc1f76cb5de32
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-12 22:16:59 +0530

    Initial commit
    jQuery-Transit: CSS3 transitions and transformations.
    Help: https://github.com/rstacruz/jquery.transit

---
+++ source/core/script/plugin/jquery/jquery.transit.js
@@ -0,0 +1,19 @@
+/*!
+ * ~~~~~~~~~~~~~~~
+ * jQuery-Transit® : CSS3 transitions and transformations.
+ * __________________________________________________________________________________
+ * @copyright : 2011 - 2014, Rico Sta. Cruz, All Rights Reserved.
+ * @help      : https://github.com/rstacruz/jquery.transit
+ * @license   : MIT
+ * @require   : jQuery
+ * @require   : requirejs
+ * __________________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * __________________________________________________________________________________
+ *
+ * --/The Heart of AMD System/-- of "MeltXpert®".
+ * __________________________________________________________________________________
+ */
