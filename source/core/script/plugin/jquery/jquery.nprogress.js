6d9b34317356d9cf459832484f770cd3cb388239
Emma Jörenberg <emma.jorenberg@gmail.com>
2016-04-12 22:04:27 +0530

    Initial commit
    NProgress: Slim progress bars for Ajax'y applications. Inspired by Google, YouTube, and Medium.
    Help: https://github.com/rstacruz/nprogress

---
+++ source/core/script/plugin/jquery/jquery.nprogress.js
@@ -0,0 +1,19 @@
+/*!
+ * ~~~~~~~~~~
+ * NProgress® : Slim progress bars for Ajax'y applications.
+ * __________________________________________________________________________________
+ * @copyright : 2013 - 2014, Rico Sta. Cruz, All Rights Reserved.
+ * @help      : https://github.com/rstacruz/nprogress
+ * @license   : MIT
+ * @require   : jQuery
+ * @require   : requirejs
+ * __________________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * __________________________________________________________________________________
+ *
+ * --/The Heart of AMD System/-- of "MeltXpert®".
+ * __________________________________________________________________________________
+ */
