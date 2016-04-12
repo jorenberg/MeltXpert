99eaa88bf1d6073a79cae2a6a2c303b8620b63cd
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-12 22:44:46 +0530

    Initial commit
    jQuery-iCheck: Highly customizable checkboxes and radio buttons for jQuery and Zepto.
    Help: https://github.com/fronteed/icheck

---
+++ source/core/script/plugin/jquery/jquery.icheck.js
@@ -0,0 +1,19 @@
+/*!
+ * ~~~~~~~~~~~~~~
+ * jQuery-iCheck® : Highly customizable checkboxes & radio buttons for jQuery.
+ * __________________________________________________________________________________
+ * @copyright : 2013, Damir Sultanov, All Rights Reserved.
+ * @help      : https://github.com/fronteed/icheck
+ * @license   : MIT
+ * @require   : jQuery
+ * @require   : requirejs
+ * __________________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * __________________________________________________________________________________
+ *
+ * --/The Heart of AMD System/-- of "MeltXpert®".
+ * __________________________________________________________________________________
+ */
