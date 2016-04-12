08741659b488abec6c8408460e0d0249cd7feb87
Emma Jörenberg <emma.jorenberg@gmail.com>
2016-04-12 22:08:37 +0530

    Initial commit
    jQuery-LoadingBar: A little jQuery plugin that will add a Youtube-like loading bar.
    Help: https://github.com/peachananr/loading-bar

---
+++ source/core/script/plugin/jquery/jquery.loadingbar.js
@@ -0,0 +1,19 @@
+/*!
+ * ~~~~~~~~~~~~~~~~~~
+ * jQuery-LoadingBar® : A little jQuery plugin that will add a Youtube-like loading bar.
+ * _____________________________________________________________________________________
+ * @copyright : 2013, Pete Rojwongsuriya, All Rights Reserved.
+ * @help      : https://github.com/peachananr/loading-bar
+ * @license   : MIT
+ * @require   : jQuery
+ * @require   : requirejs
+ * _____________________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * _____________________________________________________________________________________
+ *
+ * --/The Heart of AMD System/-- of "MeltXpert®".
+ * _____________________________________________________________________________________
+ */
