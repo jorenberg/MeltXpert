b3c1c92de166e4bff13a0e51cb70e61dc8527624
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-14 17:26:43 +0530

    Initial commit
    Path: config/autoprefixer.js
    Usage: $ npm install grunt-autoprefixer --save-dev | npm install grunt-postcss --save-dev

---
+++ config/autoprefixer.js
@@ -0,0 +1,26 @@
+/*!
+ * MeltXpert®
+ * __________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~~~~~~~
+ * AutoPrefixer :====================================================
+ * grunt-autoprefixer : https://github.com/nDmitry/grunt-autoprefixer
+ * Usage              : $ npm install grunt-autoprefixer --save-dev
+ * !
+ * grunt-postcss      : https://github.com/nDmitry/grunt-postcss
+ * Usage              : $ npm install grunt-postcss --save-dev
+ * __________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * __________________________________________________________________
+ * @date      : 14-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * __________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "MeltXpert®".
+ * __________________________________________________________________
+ */
