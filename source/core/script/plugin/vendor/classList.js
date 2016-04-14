0b0840299f5494076da897ef08e9932dc0b3e696
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-14 14:39:18 +0530

    Initial commit
    classList®: Cross-browser full element.classList implementation.
    Help: https://github.com/eligrey/classList.js | http://eligrey.com/

---
+++ source/core/script/plugin/vendor/classList.js
@@ -0,0 +1,18 @@
+/*!
+ * ~~~~~~~~~~
+ * classList® : Cross-browser full element.classList implementation.
+ * __________________________________________________________________________________
+ * @copyright : 2014 - 2016, Eli Grey, All Rights Reserved.
+ * @help      : https://github.com/eligrey/classList.js | http://eligrey.com/
+ * @license   : http://unlicense.org/
+ * @require   : requirejs
+ * __________________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * __________________________________________________________________________________
+ *
+ * --/The Heart of AMD System/-- of "MeltXpert®".
+ * __________________________________________________________________________________
+ */
