37f0875b965c745ccd8e890e294cb73ecc45c267
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-14 22:20:42 +0530

    Initial commit
    Path: config/less.js
    grunt-contrib-less: Compile LESS files to CSS.
    Usage: $ npm install grunt-contrib-less --save-dev

---
+++ config/less.js
@@ -0,0 +1,24 @@
+/*!
+ * MeltXpert®
+ * ____________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~
+ * grunt-contrib-less : https://github.com/gruntjs/grunt-contrib-less
+ * Help               : http://lesscss.org/
+ * Usage              : $ npm install grunt-contrib-less --save-dev
+ * @require           : rule/.lessrc
+ * ____________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ____________________________________________________________________
+ * @date      : 14-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * ____________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "MeltXpert®".
+ * ____________________________________________________________________
+ */
