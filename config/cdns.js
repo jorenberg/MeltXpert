4b8cf2d27b907d01f003ca4cc32873da14873d51
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-14 21:54:52 +0530

    Initial commit
    Path: config/cdns.js
    grunt-google-cdn: To replace Google CDN references.
    Usage: $ npm install grunt-google-cdn --save-dev

---
+++ config/cdns.js
@@ -0,0 +1,25 @@
+/*!
+ * MeltXpert®
+ * ____________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~~~~~~~~~~~
+ * grunt-google-cdn : https://github.com/btford/grunt-google-cdn
+ * Help             : 1. https://github.com/shahata/google-cdn-data
+ *                    2. https://github.com/shahata/cdnjs-cdn-data
+ * Usage            : $ npm install grunt-google-cdn --save-dev
+ * @require         : bower.json or component.json
+ * ____________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ____________________________________________________________________
+ * @date      : 14-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * ____________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "MeltXpert®".
+ * ____________________________________________________________________
+ */
