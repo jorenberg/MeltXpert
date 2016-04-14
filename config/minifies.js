9080376261c23bc2cf3e7f548fe682d27a74e809
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-14 20:14:29 +0530

    Initial commit
    Path: config/minifies.js
    Usage:
    1. $ npm install grunt-contrib-htmlmin --save-dev
    2. $ npm install grunt-contrib-cssmin --save-dev
    3. $ npm install grunt-contrib-imagemin --save-dev
    4. $ npm install grunt-contrib-uglify --save-dev
    5. $ npm install grunt-svgmin --save-dev
    6. $ npm install grunt-usemin --save-dev

---
+++ config/minifies.js
@@ -0,0 +1,39 @@
+/*!
+ * MeltXpert®
+ * ___________________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~~~~~~
+ * Minifier(s) : @require ====================================================
+ * --> 1.
+ * grunt-contrib-htmlmin  : https://github.com/gruntjs/grunt-contrib-htmlmin
+ * Usage              	  : $ npm install grunt-contrib-htmlmin --save-dev
+ * --> 2.
+ * grunt-contrib-cssmin   : https://github.com/gruntjs/grunt-contrib-cssmin
+ * Usage              	  : $ npm install grunt-contrib-cssmin --save-dev
+ * --> 3.
+ * grunt-contrib-imagemin : https://github.com/gruntjs/grunt-contrib-imagemin
+ * Usage              	  : $ npm install grunt-contrib-imagemin --save-dev
+ * --> 4.
+ * grunt-contrib-uglify	  : https://github.com/gruntjs/grunt-contrib-uglify
+ * Usage              	  : $ npm install grunt-contrib-uglify --save-dev
+ * --> 5.
+ * grunt-svgmin           : https://github.com/sindresorhus/grunt-svgmin
+ * Usage                  : $ npm install grunt-svgmin --save-dev
+ * --> 6.
+ * grunt-usemin           : https://github.com/yeoman/grunt-usemin
+ * Usage                  : $ npm install grunt-usemin --save-dev
+ * ___________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ___________________________________________________________________________
+ * @date      : 14-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * ___________________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "MeltXpert®".
+ * ___________________________________________________________________________
+ */
