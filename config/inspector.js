00c04edbcf6865cea6f6502e7e4c5d36a1ddcd8f
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-15 04:10:59 +0530

    Initial commit
    Path: config/inspector.js
    grunt-node-inspector: Node Inspector is a debugger interface for Node.js applications.
    Usage:
    1. $ npm install grunt-node-inspector --save-dev
    2. $ npm install node-inspector -g

---
+++ config/inspector.js
@@ -0,0 +1,25 @@
+/*!
+ * MeltXpert®
+ * ________________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~~~~~~~~~~~~~~~
+ * grunt-node-inspector : https://github.com/ChrisWren/grunt-node-inspector
+ * Usage                : $ npm install grunt-node-inspector --save-dev
+ * !
+ * node-inspector       : https://github.com/node-inspector/node-inspector
+ * Usage                : $ npm install node-inspector -g
+ * ________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ________________________________________________________________________
+ * @date      : 15-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * ________________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "MeltXpert®".
+ * ________________________________________________________________________
+ */
