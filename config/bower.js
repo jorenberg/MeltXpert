3da5a758ae498ea9fc2a1c75d1d9e650fae560e2
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-14 21:29:48 +0530

    Initial commit
    Path: config/bower.js
    grunt-wiredep: Inject Bower packages into your source code with Grunt.
    Usage: $ npm install grunt-wiredep --save-dev

---
+++ config/bower.js
@@ -0,0 +1,24 @@
+/*!
+ * MeltXpert®
+ * ____________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~
+ * Bower          : http://bower.io/
+ * grunt-wiredep  : https://github.com/stephenplusplus/grunt-wiredep
+ * Usage          : $ npm install grunt-wiredep --save-dev
+ * @require       : .bowerrc and bower.json
+ * ____________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ____________________________________________________________________
+ * @date      : 14-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * ____________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "MeltXpert®".
+ * ____________________________________________________________________
+ */
