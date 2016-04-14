df998293f8475f87a23fabe9f5bc598c6fbf14c3
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-15 02:41:59 +0530

    Initial commit
    Path: config/coffee.js
    grunt-contrib-coffee: Compile CoffeeScript files to JavaScript.
    Usage:
    1. $ npm install grunt-contrib-coffee --save-dev
    2. $ npm install grunt-coffeelint --save-dev

---
+++ config/coffee.js
@@ -0,0 +1,34 @@
+/*!
+ * MeltXpert®
+ * ___________________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~
+ * Coffee : @require =========================================================
+ * --> 1.
+ * grunt-contrib-coffee	  : https://github.com/gruntjs/grunt-contrib-coffee
+ * Usage              	  : $ npm install grunt-contrib-coffee --save-dev
+ * !
+ * coffee-script          - https://github.com/jashkenas/coffeescript
+ * ___________________________________________________________________________
+ * --> 2.
+ * grunt-coffeelint       : https://github.com/vojtajina/grunt-coffeelint
+ * Usage                  : $ npm install grunt-coffeelint --save-dev
+ * @require               : rule/.coffeelint.json
+ * !
+ * coffeelint             - https://github.com/clutchski/coffeelint
+ * coffeelint-stylish 	  - https://github.com/janraasch/coffeelint-stylish
+ * ___________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ___________________________________________________________________________
+ * @date      : 15-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * ___________________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "MeltXpert®".
+ * ___________________________________________________________________________
+ */
