386df6b8f628b30e700a9dbf41227861e3e61ccc
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-14 14:01:18 +0530

    Initial commit
    Letters®: An experimental plugin for animating SVG letters using Segment.
    Help: https://github.com/lmgonzalves/letters

---
+++ source/core/script/plugin/vendor/letters.js
@@ -0,0 +1,18 @@
+/*!
+ * ~~~~~~~~
+ * Letters®   : An experimental plugin for animating SVG letters using Segment.
+ * __________________________________________________________________________________
+ * @copyright : 2016, lmgonzalves, All Rights Reserved.
+ * @help      : https://github.com/lmgonzalves/letters
+ * @license   : MIT
+ * @require   : requirejs
+ * __________________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * __________________________________________________________________________________
+ *
+ * --/The Heart of AMD System/-- of "MeltXpert®".
+ * __________________________________________________________________________________
+ */
