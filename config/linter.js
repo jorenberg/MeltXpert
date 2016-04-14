f301caee15658c79482f0cd970aa561d294ae877
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-15 00:54:40 +0530

    Initial commit
    Path: config/linter.js
    Usage:
    1. $ npm install grunt-htmlhint --save-dev
    2. $ npm install grunt-scss-lint --save-dev
    3. $ npm install grunt-contrib-csslint --save-dev
    4. $ npm install grunt-lesslint --save-dev
    5. $ npm install grunt-contrib-jshint --save-dev

---
+++ config/linter.js
@@ -0,0 +1,41 @@
+/*!
+ * MeltXpert®
+ * ___________________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~~~~
+ * Linter(s) : @require ======================================================
+ * --> 1.
+ * grunt-htmlhint         : https://github.com/yaniswang/grunt-htmlhint
+ * Usage              	  : $ npm install grunt-htmlhint --save-dev
+ * @require               : .htmlhintrc
+ * --> 2.
+ * grunt-scss-lint        : https://github.com/ahmednuaman/grunt-scss-lint
+ * Usage                  : $ npm install grunt-scss-lint --save-dev
+ * @require               : .scss-lint.yml
+ * --> 3.
+ * grunt-contrib-csslint  : https://github.com/gruntjs/grunt-contrib-csslint
+ * Usage              	  : $ npm install grunt-contrib-csslint --save-dev
+ * @require               : .csslintrc
+ * --> 4.
+ * grunt-lesslint         : https://github.com/jgable/grunt-lesslint
+ * Usage                  : $ npm install grunt-lesslint --save-dev
+ * @require               : .csslintrc
+ * --> 5.
+ * grunt-contrib-jshint   : https://github.com/gruntjs/grunt-contrib-jshint
+ * Usage              	  : $ npm install grunt-contrib-jshint --save-dev
+ * @require               : .jshintrc
+ * ___________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ___________________________________________________________________________
+ * @date      : 15-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * ___________________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "MeltXpert®".
+ * ___________________________________________________________________________
+ */
