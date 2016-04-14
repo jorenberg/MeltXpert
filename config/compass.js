e6928e7574b81fe666b42cfc33b1d80bd63b166a
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-14 18:59:41 +0530

    Initial commit
    Path: config/compass.js
    Usage: $ npm install grunt-contrib-compass --save-dev

---
+++ config/compass.js
@@ -0,0 +1,22 @@
+/*!
+ * MeltXpert®
+ * ________________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~~~
+ * grunt-contrib-compass : https://github.com/gruntjs/grunt-contrib-compass
+ * Usage                 : $ npm install grunt-contrib-compass --save-dev
+ * ________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ________________________________________________________________________
+ * @date      : 14-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * ________________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "MeltXpert®".
+ * ________________________________________________________________________
+ */
