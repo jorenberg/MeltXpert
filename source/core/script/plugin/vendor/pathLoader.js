0d7e3d1a1e7c54c9885da75ac4ce5f79a01c9ec4
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-14 14:30:49 +0530

    Initial commit
    pathLoader®: A little JavaScript for SVG path loading.
    Help: https://gist.github.com/iamprabhat/112a49f18550eba75a2313b852ee3e7b

---
+++ source/core/script/plugin/vendor/pathLoader.js
@@ -0,0 +1,18 @@
+/*!
+ * ~~~~~~~~~~~
+ * pathLoader®: A little JavaScript for SVG path loading.
+ * __________________________________________________________________________________
+ * @copyright : 2016, Prabhat Kumar, All Rights Reserved.
+ * @help      : https://gist.github.com/iamprabhat/112a49f18550eba75a2313b852ee3e7b
+ * @license   : MIT
+ * @require   : requirejs
+ * __________________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * __________________________________________________________________________________
+ *
+ * --/The Heart of AMD System/-- of "MeltXpert®".
+ * __________________________________________________________________________________
+ */
