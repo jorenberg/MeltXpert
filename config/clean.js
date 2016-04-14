416d99301a226c12c535cdd494d61435563d974a
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-14 18:49:37 +0530

    Initial commit
    Path: config/clean.js
    Usage: $ npm install grunt-contrib-clean --save-dev

---
+++ config/clean.js
@@ -0,0 +1,22 @@
+/*!
+ * MeltXpert®
+ * ____________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~~~
+ * grunt-contrib-clean : https://github.com/gruntjs/grunt-contrib-clean
+ * Usage               : $ npm install grunt-contrib-clean --save-dev
+ * ____________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ____________________________________________________________________
+ * @date      : 14-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * ____________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "MeltXpert®".
+ * ____________________________________________________________________
+ */
