f4cd51b92c01847f8f6acca89c2994fce9755fdf
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-14 17:15:29 +0530

    Initial commit
    Path: config/assembler.js
    Usage: $ npm install assemble --save-dev

---
+++ config/assembler.js
@@ -0,0 +1,22 @@
+/*!
+ * MeltXpert®
+ * ______________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~~~
+ * Assemble   : https://github.com/assemble/assemble
+ * Usage      : $ npm install assemble --save-dev
+ * ______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ______________________________________________________________
+ * @date      : 14-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * ______________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "MeltXpert®".
+ * ______________________________________________________________
+ */
