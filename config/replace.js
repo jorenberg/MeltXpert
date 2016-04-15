ca35670c3f04a9980cd867706ff83bcfe155de6c
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-15 08:41:40 +0530

    Initial commit
    Path: config/replace.js
    grunt-string-replace: Replaces strings on files by using string or regex patterns.
    Usage: $ npm install grunt-string-replace --save-dev

---
+++ config/replace.js
@@ -0,0 +1,22 @@
+/*!
+ * MeltXpert®
+ * _____________________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~~~~~~~~~~~~~~~
+ * grunt-string-replace : https://github.com/eruizdechavez/grunt-string-replace
+ * Usage                : $ npm install grunt-string-replace --save-dev
+ * _____________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * _____________________________________________________________________________
+ * @date      : 15-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * _____________________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "MeltXpert®".
+ * _____________________________________________________________________________
+ */
