a12c50a4e8dd14820902a3e800c54140ef2a1cae
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-15 10:40:39 +0530

    Initial commit
    Path: config/watch.js
    Watch: Run predefined tasks whenever watched file patterns are added, changed or deleted.

---
+++ config/watch.js
@@ -0,0 +1,26 @@
+/*!
+ * MeltXpert®
+ * ____________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~~~~~~~
+ * Watch :=============================================================
+ * grunt-contrib-watch : https://github.com/gruntjs/grunt-contrib-watch
+ * Usage               : $ npm install grunt-contrib-watch --save-dev
+ * ---->
+ *                 HTML/SASS/LESS/CSS/JavaScript/JSON
+ * Usage               : $ grunt serve
+ * ____________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ____________________________________________________________________
+ * @date      : 15-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * ____________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "MeltXpert®".
+ * ____________________________________________________________________
+ */
