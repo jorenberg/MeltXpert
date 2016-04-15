46fa3403bf27a1cf78ed176ed336fbc76150136c
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-15 10:52:56 +0530

    Initial commit.🇮🇳©️®⇪
    Path: config/settings/watch.js
    Bump: Increment package version.

---
+++ config/settings/bump.js
@@ -0,0 +1,24 @@
+/*!
+ * MeltXpert®
+ * __________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~
+ * Bump :============================================================
+ *  |-------------- Bump package version, create tag, commit, push...
+ * grunt-bump : https://github.com/vojtajina/grunt-bump
+ * Usage      : $ npm install grunt-bump --save-dev
+ * __________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * __________________________________________________________________
+ * @date      : 15-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * __________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "MeltXpert®".
+ * __________________________________________________________________
+ */
