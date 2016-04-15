a859532ea9bdbdc7033210ddf485fab041b80846
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-15 09:01:09 +0530

    Initial commit
    Path: config/valid.js
    grunt-jsonlint: Validate JSON files from grunt.
    Usage: $ npm install grunt-jsonlint --save-dev

---
+++ config/valid.js
@@ -0,0 +1,22 @@
+/*!
+ * MeltXpert®
+ * _________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~~~~~~~~~
+ * grunt-jsonlint : https://github.com/brandonramirez/grunt-jsonlint
+ * Usage          : $ npm install grunt-jsonlint --save-dev
+ * _________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * _________________________________________________________________
+ * @date      : 15-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * _________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "MeltXpert®".
+ * _________________________________________________________________
+ */
