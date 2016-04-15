d97e9d137df3bc1b1fec408f4d9f7c4e8e51018c
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-15 09:28:10 +0530

    Initial commit
    Path: config/utils.js
    Usage:
    1. $ npm install grunt-newer --save-dev
    2. $ npm install grunt-contrib-compress --save-dev

---
+++ config/utils.js
@@ -0,0 +1,30 @@
+/*!
+ * MeltXpert®
+ * _____________________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~~
+ * Utility : @require ==========================================================
+ * --> 1.
+ * grunt-newer            : https://github.com/tschaub/grunt-newer
+ * Usage                  : $ npm install grunt-newer --save-dev
+ * !
+ * grunt-rsync            : https://github.com/jedrichards/grunt-rsync
+ * Usage                  : $ npm install grunt-rsync --save-dev
+ * --> 2.
+ * grunt-contrib-compress : https://github.com/gruntjs/grunt-contrib-compress
+ * Usage                  : $ npm install grunt-contrib-compress --save-dev
+ * _____________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * _____________________________________________________________________________
+ * @date      : 15-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * _____________________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "MeltXpert®".
+ * _____________________________________________________________________________
+ */
