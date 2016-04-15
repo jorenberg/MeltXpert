2436f09f4836ce1d40d6ae0caa5d652751e32331
Prabhat Kumar <prabhat.genome@gmail.com>
2016-04-15 10:18:30 +0530

    Initial commit
    Path: config/wait.js
    Wait: Grunt wait Task for e2e testing.

---
+++ config/wait.js
@@ -0,0 +1,21 @@
+/*!
+ * MeltXpert®
+ * ______________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~
+ * Wait       : Grunt wait Task for "e2e" testing.
+ * ______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ______________________________________________________________
+ * @date      : 15-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * ______________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "MeltXpert®".
+ * ______________________________________________________________
+ */
